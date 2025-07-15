02ccc64d28613ecdf6733d9c8902dd12c574f8a5
Deepthisai22 <saideepthianupoju@gmail.com>
2025-07-15 20:00:46 +0530

    Update app.py to bind 0.0.0.0 and use Render port

--- app.py
+++ app.py
@@ -81,4 +81,6 @@ def realworld():
 # Run the Flask App
 # -------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
